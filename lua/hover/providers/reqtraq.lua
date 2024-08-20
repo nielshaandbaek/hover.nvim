a13f1f8f711efd49623d6001ae20f545d244fd08
Niels Haandbaek <nh@daedalean.ai>
2024-08-20 11:41:33 +0200

    Added requirements.

---
+++ lua/hover/providers/reqtraq.lua
@@ -0,0 +1,35 @@
+local REQUIREMENT_PATTERN = "REQ-%u+-%u+-%d+"
+
+local function enabled()
+	-- Match 2 or more uppercase letters followed by a '-' and 1 or more digits.
+	return vim.fn.expand("<cWORD>"):match(REQUIREMENT_PATTERN) ~= nil
+end
+
+local function execute(opts, done)
+	local query = vim.fn.expand("<cWORD>"):match(REQUIREMENT_PATTERN)
+
+	local job = require("hover.async.job").job
+
+	job({ "rg", "'#+\\s+" .. query .. "\\b'", "~", "-g", "'*.md'" }, function(result)
+		if result == nil then
+			done(false)
+			return
+		end
+
+		local lines = {}
+		for line in result:gmatch("[^\r\n]+") do
+			-- Remove lines starting with \27, which is not formatted well and
+			-- is only there for help/context/suggestion lines anyway.
+			table.insert(lines, line)
+		end
+
+		done({ lines = lines, filetype = "markdown" })
+	end)
+end
+
+require("hover").register({
+	name = "Reqtraq",
+	priority = 175,
+	enabled = enabled,
+	execute = execute,
+})
